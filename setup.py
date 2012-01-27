18b2a43f09e389d0975da4e513c2a59b57203471
jefftriplett <jeff.triplett@gmail.com>
2012-01-27 16:50:51 -0600

    Update setup.py to install via module and a PEP8 cleanup.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from distutils.core import setup
 from spamish import __version__ as version
 
 setup(
-    name = 'spamish',
-    version = version,
-    description = 'Spam filtering library for Akismet and TypePad services',
-    author = 'Lefora',
-    author_email = 'samuel@lefora.com',
-    url = 'http://github.com/samuel/python-spamish',
-    packages = ['spamish'],
-    classifiers = [
+    name='spamish',
+    version=version,
+    description='Spam filtering library for Akismet and TypePad services',
+    author='Lefora',
+    author_email='samuel@lefora.com',
+    url='http://github.com/samuel/python-spamish',
+    py_modules=['spamish'],
+    classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
